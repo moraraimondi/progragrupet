5114e46af563ddcaabaaa53b9f094dcd90d80538
mora <moraraimondi@gmail.com>
2020-05-26 14:51:08 -0300

    usuarios pero no funca :/

--- proyectoSeries/controllers/seriesController.js
+++ proyectoSeries/controllers/seriesController.js
@@ -1,24 +1,39 @@
+let db = require('../database/models/index'); // la base de datos
+let bcrypt = require('bcryptjs');
+let op = db.Sequelize.Op;
+
 let seriesController = {
     home: function(req, res){
-        res.render('home')
+        res.render('home') //lleva a home
     },
     generos: function(req, res){
-        res.render('seriesGenres')
+        res.render('seriesGenres')//lleva al listado de generos
     },
     buscador: function(req,res){
-        res.render('buscador')
+        res.render('buscador')// lleva al buscador
     },
     resultado: function(req,res){
-        res.render('resultadoBusqueda')
+        res.render('resultadoBusqueda')// lleva a los resultados de la busqueda
     },
     detalle: function(req, res){
-        res.render('detalle')
+        res.render('detalle') // lleva al detalle de una serie
     },
     porGenero: function(req,res){
-        res.render('seriesByGenre')
+        res.render('seriesByGenre')// lleva a las series por genero
     },
     login: function(req,res){
-        res.render('login')
+        res.render('login') // lleva al login
+    },
+    validar: function(req, res){ // crea el usuario con lo que se llena en el form
+        let encriptada = bcrypt.hashSync(req.body.password, 10); //encripta los datos de la password
+        let usuario ={ //toma los datos del form
+            usuario: req.body.usuario,
+            email: req.body.email,
+            password: encriptada,
+            birthday: req.body.birthday
+        }
+        db.Usuarios.create(usuario) // crea los usuarios en la bd
+        res.redirect('/series/home') // te lleva a home cuando apretas el boton
     }
 }
 
--- proyectoSeries/routes/rutaSeries.js
+++ proyectoSeries/routes/rutaSeries.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-let seriesController = require('../controllers/seriesController');
-
+let seriesController = require('../controllers/seriesController'); 
 /* GET home page. */
 router.get('/home', seriesController.home);
 router.get('/generos', seriesController.generos);
@@ -9,6 +8,7 @@ router.get('/buscador', seriesController.buscador);
 router.get('/resultado', seriesController.resultado);
 router.get('/detalle', seriesController.detalle);
 router.get('/porGenero', seriesController.porGenero);
-router.get('/login', seriesController.login);
+router.get('/login', seriesController.login); //MUESTRA el login
+router.post('/login', seriesController.validar);
 
 module.exports = router;
